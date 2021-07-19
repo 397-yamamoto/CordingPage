5cb5cb70040ffe735e1adcb5b9d27902bd776016
yamamoto <yamamoto.yuka@compass-japan.com>
2021-07-19 15:11:38 +0900

    jQ修正

--- js/script.js
+++ js/script.js
@@ -15,63 +15,15 @@ $burger.on("click", function () {
 const elements = document.querySelectorAll(".fade-animation");
 document.addEventListener("scroll", function () {
     for (let i = 0; i < elements.length; i++) {
-        const element = elements[i].getBoundingClientRect().top //くっつくと0　なるほど！！！！
-            + elements[i].clientHeight * 0.6; //今上から見えてる画面からの距離 ６割見えたら。
+        const element = elements[i].getBoundingClientRect().top 
+            + elements[i].clientHeight * 0.6; //今上から見えてる画面からの距離 ６割
         
         // console.log(element);
-        if (window.innerHeight > element) { //現在のブラウザの高さを取得
+        if (window.innerHeight > element) { 
             elements[i].classList.add("show");
         }
     }//for
 });
-
-
-// // headerClone
-// $(".header").each(function () {
-//     let $win = $(window),
-//         $header = $(this),
-//         headerBottom = $(".fv").offset().top + $(".fv").outerHeight();
-//     let $headerClone = $header.contents().clone(),
-//         $headerCloneBox = $('<div class"header-clone"></div>');
-//     $headerCloneBox.append($headerClone);
-//     $headerCloneBox.appendTo("body");
-
-//     $win.on("scroll", function () {
-//         if ($win.scrollTop() > headerBottom) {
-//             $headerCloneBox.addClass("open");
-//         } else {
-//             $headerCloneBox.removeClass("open");
-//         }
-//     });
-// });//each
-
-    // panheader
-    $(".header").each(function(){
-        let $win =$(window),
-            $header =$(this),
-            headerbottom =$header.offset().top +$header.outerHeight();
-
-        let $headerClone =$header.contents().clone(),
-            $headerCloneBox = $('<div class="header-clone"></div>');
-        
-        $headerCloneBox.append($headerClone);
-        $headerCloneBox.appendTo("body");
-
-        $win.on("scroll",function(){
-
-            if($win.scrollTop()>headerbottom){
-                $headerCloneBox.addClass("ok");
-                $headerCloneBox.find("a").addClass("ok");
-            }else{
-                $headerCloneBox.removeClass("ok");
-                $headerCloneBox.find("a").removeClass("ok");
-            }
-
-        });//on
-        $win.trigger("scroll");
-    });///each
-
-
 
 
 
